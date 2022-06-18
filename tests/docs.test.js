4eae80480d2feb8662230f91e208d6d3adeba788
Rukanda Faridsi <rukanda.faridsi@bukalapak.com>
2022-06-18 16:05:36 +0700

    Add test

--- tests/docs.test.js
+++ tests/docs.test.js
@@ -21,8 +21,8 @@ function collectDirToCheck(dirPath) {
 
 collectDirToCheck(dirToCheck[0][0]);
 
-describe("Check for duplicate ID on each Areas File", () => {
-  test.each(dirToCheck)("Check for duplicate ID on %s", (dir) => {
+describe("Check for duplicate data on each areas.json file", () => {
+  test.each(dirToCheck)("Check for duplicate 'id' on %s", (dir) => {
     const data = require(`.${dir}areas.json`);
 
     expect(data).toHaveProperty("areas");
@@ -30,6 +30,18 @@ describe("Check for duplicate ID on each Areas File", () => {
 
     const ids = data.areas.map((area) => area.id);
     const isDuplicate = ids.some((id, idx) => ids.indexOf(id) !== idx);
+
+    expect(isDuplicate).toBe(false);
+  });
+
+  test.each(dirToCheck)("Check for duplicate 'name' on %s", (dir) => {
+    const data = require(`.${dir}areas.json`);
+
+    expect(data).toHaveProperty("areas");
+    expect(data.areas.length).toBeGreaterThan(0);
+
+    const names = data.areas.map((area) => area.name);
+    const isDuplicate = names.some((name, idx) => names.indexOf(name) !== idx);
 
     expect(isDuplicate).toBe(false);
   });
